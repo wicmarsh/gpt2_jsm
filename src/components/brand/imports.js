3d155c470a66f8499bdc040dabfddbbcc214f925
wycliffe <wicmarsh@users.noreply.github.com>
2023-03-06 19:17:29 +0000

    added feature components

---
+++ src/components/brand/imports.js
@@ -0,0 +1,13 @@
+import google from '../../'assests/google.png
+import google from '../../'assests/slack.png
+import google from '../../'assests/atlassian.png
+import google from '../../'dropbox/google.png
+import google from '../../'assests/shopify.png
+
+export {
+  google,
+  slack,
+  atlassian,
+  dropbox,
+  shopify,
+}
--- src/containers/whatGPT2/WhatGPT2.jsx
+++ src/containers/whatGPT2/WhatGPT2.jsx
@@ -3,8 +3,19 @@ import './whatGPT2.css';
 
 const WhatGPT2 = () => {
   return (
-    <div>
-      WhatGPT2
+    <div className="gpt2__whatgpt2 section__margin" id="whpt2">
+      <div className="gpt2__whatgpt2-feature">
+        <feature title='What is GPT-2' text='We so opinion friends me message friendly bachelor entrance to on by.'/>
+      </div>
+      <div className="gpt2__whatgpt2-heading">
+        <h1 className="gradient-text">The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt2__whatgpt2-container">
+        <feature title='Chatbots' text='front do of plate oh ought.'/>
+        <feature title='Knowledgebase' text='As put impossible own apartments b'/>
+        <feature title='Education=' text='As put impossible own apartments'/>
+      </div>
     </div>
   )
 }
