0dd4890ff66194d55c96fd04e4c27a8f3ed6e998
wycliffe <wicmarsh@users.noreply.github.com>
2023-05-15 22:34:58 +0000

    Update 16 files

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -7,22 +7,22 @@ const Header = () => {
   return (
     <div className="gpt2__header section__padding" id="home">
       <div className="gpt2__header-content">
-        <h1 className="gradient__text">Let's Build Something amazing with GPT-2 OpenAI</h1>
-        <p>Yet bed any for travelling asstance</p>
+        <h1 className="gradient__text">Let's Build Something Amazing with GPT-2 OpenAI</h1>
+        <p>Yet bed any for travelling assistance</p>
         <div className="gpt2__header-content__input">
           <input type="email" placeholder="Your Email Address" />
           <button type="button">Get Started</button>
+        </div>
+        <div className="gpt2__header-content__people">
+          <img src={people} alt="people" />
+          <p>1,600 people requested access in the last 24 hours</p>
+        </div>
       </div>
-
-      <div className="gpt2__header-content__people"></div>
-        <img src={people} alt="people"/>
-        <p>1,600 people requested access a vist in last 24 hours</p>
+      <div className="gpt2__header-image">
+        <img src={ai} alt="ai" />
       </div>
-      <div className="gpt2__header-image"></div>
-        <img src={ai} alt="ai"/>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
